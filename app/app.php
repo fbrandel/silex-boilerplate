0b823748b391773e8f096d532d9fcd33025af7e8
Florian Brandel <mail@fbrandel.de>
2011-09-24 19:56:53 +0200

    Added Symfony Component Yaml to load config file

--- app/app.php
+++ app/app.php
@@ -1,11 +1,18 @@
 <?php
 require __DIR__.'/bootstrap.php';
 
+use Symfony\Component\Yaml\Yaml;
+
 $app = new Silex\Application();
-$app['debug'] = true;
+
+// Configuration
+$app['autoloader']->registerNamespace("Symfony\Component\Yaml", __DIR__."/../vendor");
+$app['config'] = Yaml::parse(__DIR__."/config/config.yml");
+
+$app['debug'] = $app['config']['debug'];
 
 // Extensions
-$app->register(new Silex\Extension\TwigExtension(), array(
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path'       => __DIR__.'/views',
     'twig.class_path' => __DIR__.'/../vendor/Twig/lib',
 ));
